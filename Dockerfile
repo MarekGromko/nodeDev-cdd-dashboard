d735bfca17a838800ae62f0070e3659964dba88e
Marek Gromko <marek.gromko@hotmail.ca>
2025-12-13 20:12:35 -0500

    build: docker deploy settinges

---
+++ Dockerfile
@@ -0,0 +1,14 @@
+FROM node:20-alpine
+
+WORKDIR /app
+
+COPY package*.json ./
+
+RUN npm ci
+
+COPY . .
+
+EXPOSE 8080
+EXPOSE 8443
+
+CMD ["npm", "run", "dev"]
--- vite.config.ts
+++ vite.config.ts
@@ -5,20 +5,31 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite';
 
+
+let https: any = false;
+if (process.env.VITE_USE_HTTPS === 'true') {
+    https = {
+        key:  process.env.VITE_HTTPS_KEY_PATH,
+        cert: process.env.VITE_HTTPS_CERT_PATH,
+    }
+}
+
+
 // https://vite.dev/config/
 export default defineConfig({
-  server: {
-    port: Number(process.env.VITE_SERVER_PORT) ?? 3000,
-    proxy: {
-      '/api': {
-        target: process.env.VITE_API_URL,
-        changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api/, '/api'),
-      },
+    server: {
+        port: Number(process.env.VITE_SERVER_PORT) ?? 8080,
+        https,
+        proxy: {
+            '/api': {
+                target: process.env.VITE_API_URL,
+                changeOrigin: true,
+                rewrite: (path) => path.replace(/^\/api/, '/api'),
+            },
+        },
     },
-  },
-  plugins: [
-    react(),
-    tailwindcss(),
-  ],
+    plugins: [
+        react(),
+        tailwindcss(),
+    ],
 })
